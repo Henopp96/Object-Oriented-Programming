88ea4a372c2dbb51371a582523f853e5522afc1f
Henopp96 <58237336+Henopp96@users.noreply.github.com>
2020-06-09 15:55:28 -0600

    Add files via upload

--- Chapter10/Implementation10_3/src/TestMyInteger.java
+++ Chapter10/Implementation10_3/src/TestMyInteger.java
@@ -2,11 +2,19 @@
 public class TestMyInteger {
 public static void main(String [] args) {
 	MyInteger i1 = new MyInteger(20);
-	System.out.println(i1.isEven());
-	System.out.println(i1.isOdd());
-	System.out.println(i1.isPrime());
+	MyInteger i2 = new MyInteger(15);
+	System.out.println("Testing isEven: " + i1.isEven());
+	System.out.println("Testing isOdd: " + i1.isOdd());
+	System.out.println("Testing isPrime: " + i1.isPrime());
 	System.out.println(i1.parseInt("915"));
 	char[] num = { '1' , '2' , '9' };
 	System.out.println(i1.parseInt(num));
+	System.out.println("Testing equals(int): " + i1.equals(19));
+	System.out.println("Testing equals(int): " + i1.equals(20));
+	System.out.println("Testing isOdd(MyInteger): " + MyInteger.isOdd(34));
+	System.out.println("Testing isEven(MyInteger): " + MyInteger.isEven(34));
+	System.out.println("Testing isPrime(MyInteger): " + MyInteger.isPrime(31));
+	System.out.println("Testing getValue: " + i1.getValue());
+	System.out.println("Testing equals(MyInteger): " + i1.equals(i2));
 }
 }

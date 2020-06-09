bc5509ada28b1b7f90daa34160f93bbdcf9ef442
Henopp96 <58237336+Henopp96@users.noreply.github.com>
2020-06-09 11:15:02 -0600

    Create Exercise10_3.java

---
+++ Chapter10/Exercise10_3.java
@@ -0,0 +1,26 @@
+
+public class Implementation10_3 {
+	public static void main(String[] args) {
+		
+	}
+}
+	class MyInteger {
+		int value = 1;
+		
+		MyInteger(){
+			
+		}
+		int getValue() {
+			return value;
+		}
+		boolean isEven() {
+			return true;
+		}
+		boolean isOdd() {
+			return true;
+		}
+		boolean isPrime() {
+			return true;
+		}
+		
+	}
